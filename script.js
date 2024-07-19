5d5dbc49c6ed37516bbc7da79ea62d11c34c4175
g-birocci <gabrielbirocci@hotmail.com>
2024-07-19 11:49:23 -0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(".custom-carousel").owlCarousel({
     autoWidth: true,
-    loop: false
+    loop: false,
+    dots: false
   });
   $(document).ready(function () {
     $(".custom-carousel .item").click(function () {
@@ -8,3 +9,22 @@ $(".custom-carousel").owlCarousel({
       $(this).toggleClass("active");
     });
   });
+
+  const btnMob = document.getElementById('btn-mob');
+
+function toggleMenu(event){
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMob.addEventListener('click', toggleMenu);
+btnMob.addEventListener('touchstart', toggleMenu);
+
